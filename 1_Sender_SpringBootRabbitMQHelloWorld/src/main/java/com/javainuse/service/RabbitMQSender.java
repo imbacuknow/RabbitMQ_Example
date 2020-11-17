1034818e113dc054e6d77469d24c2924d61ac5c6
imbac <nwsr7705zx@gmail.com>
2020-11-17 09:28:12 +0700

    add sender

---
+++ 1_Sender_SpringBootRabbitMQHelloWorld/src/main/java/com/javainuse/service/RabbitMQSender.java
@@ -0,0 +1,31 @@
+package com.javainuse.service;
+
+import org.json.simple.JSONObject;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.javainuse.model.Employee;
+
+@Service
+public class RabbitMQSender {
+
+	@Autowired
+	private AmqpTemplate amqpTemplate;
+
+	@Value("${javainuse.rabbitmq.exchange}") // กำหนดค่าให้ตัวแปร
+	private String exchange;
+
+	@Value("${javainuse.rabbitmq.routingkey}")
+	private String routingkey;
+	String kafkaTopic = "java_in_use_topic";
+
+	public void send(Employee company) {
+		JSONObject jo = new JSONObject();
+		jo.put("empId", company.getEmpId());
+		jo.put("empName", company.getEmpName());
+		amqpTemplate.convertAndSend(exchange, routingkey, jo);
+		System.out.println("Send msg = " + jo);
+	}
+}
